06781994b0554dc797693da9a44b3dde1e20cdc0
van-xps-ubuntu <pvphat25@gmail.com>
2024-11-14 12:11:23 -0500

    draft finish nfl games

--- back-end/controllers/nflController.js
+++ back-end/controllers/nflController.js
@@ -6,7 +6,7 @@ const getNFLData = async (req, res) => {
     try {
         const data = await (await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard" + param)).json()
         const events = data['events']
-        res.status(200).json(events)
+        res.status(200).json(processData(events))
     } catch (err) {
         res.status(501).json({ 'message': err })
         console.log(err);
@@ -14,4 +14,41 @@ const getNFLData = async (req, res) => {
     }
 }
 
+const processData = (events) => {
+    var res = []
+    for (var e of events) {
+        var teams = []
+        for (var team of e['competitions'][0]['competitors']) {
+            teams.push({
+                'homeAway': team['homeAway'],
+                'name': team['team']['name'],
+                'abbreviation': team['team']['abbreviation'],
+                'displayName': team['team']['displayName'],
+                'shortDisplayname': team['team']['shortDisplayName'],
+                'color': team['team']['color'],
+                'alternateColor': team['team']['alternateColor'],
+                'logo': team['team']['logo'],
+                'score': team['team']['score']
+            })
+        }
+
+        res.push({
+            'id': e['id'],
+            'name': e['name'],
+            'shortName': e['shortName'],
+            'week': e['week']['number'],
+            'status': {
+                'clock': e['status']['clock'],
+                'displayClock': e['status']['displayClock'],
+                'period': e['status']['period'],
+                'type': e['status']['type']['name'],
+                'description': e['status']['type']['description'],
+                'detail': e['status']['type']['detail'],
+                'shortDetail': e['status']['type']['shortDetail']
+            },
+            'teams': teams
+        })
+    }
+    return res
+}
 module.exports = { getNFLData }
