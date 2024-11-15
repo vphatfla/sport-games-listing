925e57cdac1b18b1501eae38a7a4d6f7a69845e1
Dominic Z <dmnzhang@gmail.com>
2024-11-14 21:54:12 -0500

    nlb and nba data

--- back-end/routes/gameRoutes.js
+++ back-end/routes/gameRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const { getNFLData } = require('../controllers/nflController')
+const { getNLBData } = require('../controllers/nlbController')
+const { getNBAData } = require('../controllers/nbaController')
 
 const router = express.Router()
 
 router.get('/nfl-games', getNFLData);
+router.get('/nlb-games', getNLBData);
+router.get('/nba-games', getNBAData);
 
 module.exports = router
