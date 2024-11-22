027b801b16b7401968fda5201bb082dd114e7448
Van Phan Mac Air <pvphat25@gmail.com>
2024-11-22 15:06:19 -0500

    finished docker compose

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -5,8 +5,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
+  const DB_HOST = process.env.DB_HOST;
+  const MONGO_URL = process.env.MONGO_URL;
+    
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    if (DB_HOST !== undefined && DB_HOST !== null && DB_HOST !== "")
+        await mongoose.connect(process.env.DB_HOST);
+    else
+        await mongoose.connect(process.env.MONGO_URL);
+
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
