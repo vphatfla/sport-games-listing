99d3e37825cca0c921332eb580704a64126bb472
Van Phan Mac Air <pvphat25@gmail.com>
2024-11-24 15:53:35 -0500

    add static ip address

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# main.tf
 provider "aws" {
   region = "us-east-2"
 }
@@ -7,7 +8,6 @@ variable "instance_name" {
   default     = "sport-listing-instance"
 }
 
-# main.tf
 resource "aws_instance" "sport_listing_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -17,24 +17,6 @@ resource "aws_instance" "sport_listing_instance" {
   tags = {
     Name = var.instance_name
   }
-
-}
-
-# data.tf
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical's AWS account ID for Ubuntu
 }
 
 resource "aws_security_group" "sport_listing_sg" {
@@ -80,10 +62,31 @@ resource "aws_security_group" "sport_listing_sg" {
   }
 }
 
+resource "aws_eip" "sport_listing_eip" {
+  instance = aws_instance.sport_listing_instance.id
+  domain      = "vpc"
+}
+
+# data.tf
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical's AWS account ID for Ubuntu
+}
 
 # outputs.tf
 output "instance_public_ip" {
-  value = aws_instance.sport_listing_instance.public_ip
+  value = aws_eip.sport_listing_eip.public_ip
 }
 
 output "instance_id" {
