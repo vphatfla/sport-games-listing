c7c34337ea2bb3727fe06682e2959c7824cd756a
Van Phan Mac Air <pvphat25@gmail.com>
2024-11-24 13:50:24 -0500

    simple ec2 to run

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,46 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-2"
 }
 
-# Backend Module
-module "backend" {
-  source          = "./backend.tf"
-  backend_image   = var.backend_image
-  cluster_id      = var.cluster_id
-  subnet_ids      = var.subnet_ids
-  security_groups = [var.backend_security_group]
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  default     = "sport-listing-instance"
 }
 
-# Frontend Module
-module "frontend" {
-  source                = "./frontend.tf"
-  frontend_bucket_name  = var.frontend_bucket_name
-  environment           = var.environment
+# main.tf
+resource "aws_instance" "sport_listing_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+
 }
 
-# ALB Module
-module "alb" {
-  source               = "./alb.tf"
-  vpc_id               = var.vpc_id
-  subnet_ids           = var.subnet_ids
-  backend_target_group = module.backend.target_group_arn
+# data.tf
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical's AWS account ID for Ubuntu
 }
 
-# Route 53 DNS Module
-module "dns" {
-  source          = "./dns.tf"
-  domain_name     = var.domain_name
-  cloudfront_url  = module.frontend.cloudfront_url
-  backend_alb_dns = module.alb.alb_dns_name
+# outputs.tf
+output "instance_public_ip" {
+  value = aws_instance.sport_listing_instance.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.sport_listing_instance.id
 }
 
